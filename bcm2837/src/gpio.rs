36f3fdb8f3f9aa3a7d4cb726c97596a4b26f8395
Jon Lamb <lamb.jon.io@gmail.com>
2018-12-01 09:52:31 -0800

    Add SPI0 GPIO support

--- bcm2837/src/gpio.rs
+++ bcm2837/src/gpio.rs
@@ -6,6 +6,30 @@ use register::mmio::ReadWrite;
 register_bitfields! {
     u32,
 
+    /// GPIO Function Select 0
+    GPFSEL0 [
+        /// Pin 9
+        FSEL9 OFFSET(27) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            SPI0_MISO = 0b100 // SPI0 MISO - Alternate function 0
+        ],
+
+        /// Pin 8
+        FSEL8 OFFSET(24) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            SPI0_CE0_N = 0b100 // SPI0 chip select 0 - Alternate function 0
+        ],
+
+        /// Pin 7
+        FSEL7 OFFSET(21) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            SPI0_CE1_N = 0b100 // SPI0 chip select 1 - Alternate function 0
+        ]
+    ],
+
     /// GPIO Function Select 1
     GPFSEL1 [
         /// Pin 15
@@ -22,6 +46,20 @@ register_bitfields! {
             Output = 0b001,
             TXD0 = 0b100, // UART0     - Alternate function 0
             TXD1 = 0b010  // Mini UART - Alternate function 5
+        ],
+
+        /// Pin 11
+        FSEL11 OFFSET(3) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            SPI0_SCLK = 0b100 // SPI0 clock - Alternate function 0
+        ],
+
+        /// Pin 10
+        FSEL10 OFFSET(0) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            SPI0_MOSI = 0b100 // SPI0 MOSI - Alternate function 0
         ]
     ],
 
@@ -37,6 +75,48 @@ register_bitfields! {
         PUDCLK14 OFFSET(14) NUMBITS(1) [
             NoEffect = 0,
             AssertClock = 1
+        ],
+
+        /// Pin 13
+        PUDCLK13 OFFSET(13) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        /// Pin 12
+        PUDCLK12 OFFSET(12) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        /// Pin 11
+        PUDCLK11 OFFSET(11) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        /// Pin 10
+        PUDCLK10 OFFSET(10) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        /// Pin 9
+        PUDCLK9 OFFSET(9) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        /// Pin 8
+        PUDCLK8 OFFSET(8) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        /// Pin 7
+        PUDCLK7 OFFSET(7) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
         ]
     ]
 }
@@ -46,10 +126,10 @@ pub const PADDR: u64 = MMIO_BASE + 0x0020_0000;
 #[allow(non_snake_case)]
 #[repr(C)]
 pub struct RegisterBlock {
-    __reserved_0: u32,                                  // 0x00
-    pub GPFSEL1: ReadWrite<u32, GPFSEL1::Register>,     // 0x04
-    __reserved_1: [u32; 35],                            // 0x08
-    pub GPPUD: ReadWrite<u32>,                          // 0x94
+    pub GPFSEL0: ReadWrite<u32, GPFSEL0::Register>, // 0x04
+    pub GPFSEL1: ReadWrite<u32, GPFSEL1::Register>, // 0x04
+    __reserved_1: [u32; 35],                        // 0x08
+    pub GPPUD: ReadWrite<u32>,                      // 0x94
     pub GPPUDCLK0: ReadWrite<u32, GPPUDCLK0::Register>, // 0x98
 }
 
--- spi-enc28j60-eth-example/README.md
+++ spi-enc28j60-eth-example/README.md
@@ -8,3 +8,7 @@ a ENC28J60 SPI Ethernet controller.
 - [enc28j60 Rust crate](https://crates.io/crates/enc28j60)
 - [SunFounder board](https://www.sunfounder.com/enc28j60-ethernet-lan-network-module.html)
 - [ENC28J60](https://www.microchip.com/wwwproducts/en/en022889)
+
+## Links
+
+- https://www.raspberrypi.org/documentation/hardware/raspberrypi/spi/README.md
